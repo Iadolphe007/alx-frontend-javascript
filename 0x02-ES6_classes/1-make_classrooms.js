209a598f4483749f3242be2a44c1b179af1edeee
Iadolphe007 <iadolphe007@gmail.com>
2024-01-04 14:16:59 +0000

    new instance

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,12 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
   const rooms = [];
-  
+
   const room1 = new ClassRoom(19);
   const room2 = new ClassRoom(20);
   const room3 = new ClassRoom(34);
   rooms.push(room1, room2, room3);
 
-  return rooms
+  return rooms;
 }
