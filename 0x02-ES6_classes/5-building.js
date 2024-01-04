6a88989ef8f70d5be71efabbe5e2195d3992b09f
Iadolphe007 <iadolphe007@gmail.com>
2024-01-04 17:31:37 +0000

    abstract

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -5,7 +5,7 @@ export default class Building {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
-  } 
+  }
 
   get sqft() {
     return this._sqft;
