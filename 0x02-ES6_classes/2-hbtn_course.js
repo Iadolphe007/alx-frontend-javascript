12022e429747e35e72530fefddb6683132c7e703
Iadolphe007 <iadolphe007@gmail.com>
2024-01-04 15:31:29 +0000

    get n set

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,58 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = this._validateString(name, 'name');
+    this._length = this._validateNumber(length, 'length');
+    this._students = this._validateArrayOfStrings(students, 'students');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = this._validateString(value, 'name');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = this._validateNumber(value, 'length');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._students = this._validateArrayOfStrings(value, 'students');
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      console.error(`${attributeName} must be a string.`);
+      return undefined;
+    }
+    return value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _validateNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      console.error(`${attributeName} must be a number.`);
+      return undefined;
+    }
+    return value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _validateArrayOfStrings(value, attributeName) {
+    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
+      console.error(`${attributeName} must be an array of strings.`);
+      return undefined;
+    }
+    return value;
+  }
+}
