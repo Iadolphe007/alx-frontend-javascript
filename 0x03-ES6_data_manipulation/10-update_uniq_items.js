93fdc9b89fe7bf55193412aa3b0d429300d28450
Iadolphe007 <iadolphe007@gmail.com>
2024-01-09 16:37:58 +0000

    task10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(data) {
+  if (!(data instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  data.forEach((value, key) => {
+    if (value === 1) {
+      data.set(key, 100);
+    }
+  });
+  return data;
+}
