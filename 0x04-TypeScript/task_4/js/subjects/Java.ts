eed5d82d2efb4ff2328ffe9bbcd29df570fa9b60
Iadolphe007 <iadolphe007@gmail.com>
2024-01-11 10:28:53 +0000

    task9

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,22 @@
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class java extends Subject{
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java'
+    }
+
+    getAvailableTeacher(): string {
+      const experience = this.teacher.experienceTeachingJava;
+      if (experience > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return 'No available teacher';
+      }
+    }
+  }
+}
